1a32eb5f361d560b7f8141b25fb11d8fd10d1c9e
ArturasJacevicius <a.jacevicius@gmail.com>
2022-10-03 17:10:47 +0300

    Task 9 completed

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,19 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+  }
+  wasExpensive() {
+    return this.budget > 100000000;
+  }
+}
+
+const testMovie = new Movie("Star Wars", "George Lucas", 500000000);
+
+console.log(testMovie);
+console.log(testMovie.wasExpensive());
