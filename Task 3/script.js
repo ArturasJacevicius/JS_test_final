62e2be8f3182c27c28dc5ad046b23b9ebc60a86a
ArturasJacevicius <a.jacevicius@gmail.com>
2022-10-03 16:57:02 +0300

    Task 3 completed

--- Task 3/script.js
+++ Task 3/script.js
@@ -11,4 +11,40 @@ Pastaba: Sukurta kortelė, kurioje yra pateikiama vartotojo informacija, turi
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+const ENDPOINT = "https://api.github.com/users";
+const button = document.getElementById("btn");
+const output = document.getElementById("output");
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  button.style.display = "none";
+  document.getElementById("message").style.display = "none";
+  getUsers();
+});
+
+const getUsers = async () => {
+  try {
+    const response = await fetch(ENDPOINT);
+    const data = await response.json();
+    console.log(data);
+    for (let i = 0; i < data.length; i++) {
+      const card = document.createElement("div");
+      const loginValue = document.createElement("h3");
+      loginValue.textContent = `USER: ${data[i].login}`;
+      const avatar = document.createElement("img");
+      avatar.setAttribute("src", data[i].avatar_url);
+      avatar.setAttribute("width", "200px");
+      avatar.setAttribute("height", "200px");
+      card.append(loginValue, avatar);
+      card.style.border = "5px solid black";
+      output.append(card);
+    }
+    output.style.display = "flex";
+    output.style.flexWrap = "wrap";
+    output.style.justifyContent = "center";
+    output.style.gap = "2rem";
+    output.style.alignContent = "space-around";
+  } catch (error) {
+    alert(error);
+  }
+};
